3f767a293e8a279aeca0551b8a77a3c203bbdc6f
Albert <guang7226961@naver.com>
2022-10-28 07:39:35 +0900

    요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,24 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
-    @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
-    }
+	@Test
+	void replace() {
+		String actual = "abc".replace("b", "d");
+		assertThat(actual).isEqualTo("adc");
+	}
+
+	@Test
+	void split_is_working_well() {
+		String testNumber1 = "1,2";
+		String[] split1 = testNumber1.split(",");
+		assertThat(split1).contains("1", "2");
+
+		String testNumber2 = "1";
+		String[] split2 = testNumber2.split(",");
+		assertThat(split2).contains("1");
+	}
 }
